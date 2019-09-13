4724b73efce72f0909c4bf5c4f3794f3abf584f7
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-09-13 12:25:18 +0800

    Pages component

---
+++ src/components/Pages/Page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="page">
+    <ul>
+      <li>{{ page.title }}</li>
+      <li>{{ page.body }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["page"],
+  data() {
+    return {};
+  }
+};
+</script>
